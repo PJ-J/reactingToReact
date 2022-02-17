65785692b3510c600883e3e883b011332be22ff1
PJ Jones <pj.jones4000.com>
2022-02-17 13:38:48 -0600

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Greeter from "./components/Greeter.jsx";
 
 const App = () => {
-
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    console.log('effect running!')
+    console.log("effect running!");
     setTimeout(() => {
       setLoaded(true);
     }, 3000);
-}, [loaded]);
+  }, [loaded]);
 
   const handleToggle = () => {
     setLoaded(!loaded);
-  }
+  };
 
   if (!loaded) {
     return (
       <div>
-      'Website loading...'
-    <button onClick={handleToggle}>Toggle</button>
-    </div>
+        'Website loading...'
+        <button onClick={handleToggle}>Toggle</button>
+      </div>
     );
   } else {
-
-  return (
-    <div>
-      <Greeter name="PJ" phrase="Hotdog" />
-    <Greeter name="Alyssa" phrase="Wowee" />
-    <Greeter name="Jimjam" phrase="Shut up" />
-    <input value={username} onChange={e => setUsername(e.target.value)}/>
-    <p>You are logging in as: {username}</p>
-    <button onClick={handleToggle}>Toggle</button>
-    </div>
-  );
-  };
+    return (
+      <div>
+        <Greeter name="PJ" phrase="Hotdog" />
+        <Greeter name="Alyssa" phrase="Wowee" />
+        <Greeter name="Jimjam" phrase="Shut up" />
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
+        <p>You are logging in as: {username}</p>
+        <button onClick={handleToggle}>Toggle</button>
+      </div>
+    );
+  }
 };
 
 export default App;
